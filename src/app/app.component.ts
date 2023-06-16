4fbcf608095c366cc85b58104791d542fb1bb633
Sissoko <m.sissoko7035@gmail.com>
2023-06-16 23:51:28 +0000

    Design main page and generate connexion page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { MenuController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private menuCtrl: MenuController,
+    private navCtrl: NavController
+  ) {}
+
+  openPage(page: string) {
+    // Ferme le menu
+    this.menuCtrl.close();
+
+    // Navigue vers la page sélectionnée
+    this.navCtrl.navigateForward(page);
+  }
 }
+
