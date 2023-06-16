5a4d38037b6d556a1869934eb725ee9dabcc66a1
Sissoko <m.sissoko7035@gmail.com>
2023-06-16 21:50:19 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nanidelice',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
